d43d7af004f385924ae67d769958814d72541ab8
Casey Pas <casey@sudo.nl>
2024-08-25 21:45:36 +0200

    first commit

---
+++ myapi.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI, Path
+from typing import Optional
+
+app = FastAPI()
+
+students = {
+    1: {
+        "name": "john",
+        "age": 17,
+        "class": "year 12"
+    }
+}
+
+@app.get("/")
+def index():
+    return {"name": "Casey"}
+
+@app.get("/get-student/{student_id}")
+def get_student(student_id: int = Path(..., description="The ID of the student you want to view", gt=0, lt=3)):
+    return students[student_id]
+
+@app.get("/get-student-by-name")
+def get_student(test: int, name: Optional[str] = None):
+    for student_id in students:
+        if students[student_id]["name"] == name:
+            return students[student_id]
+        return {"Data": "Not found"}
